6da7a836581959e82e64b871c70342465cd72e4d
Karly-22 <karly.holloway@hotmail.com>
2022-07-05 12:45:22 +0100

    add modal function to show more info when pressed

--- src/screens/ChoreCard.jsx
+++ src/screens/ChoreCard.jsx
@@ -1,34 +1,43 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Button, Card, Checkbox, List } from "react-native-paper";
+import { Pressable, Button, Card, Checkbox, List } from "react-native-paper";
 import { patchChoreIsCompleted } from "../utils/api";
+import ChoreModal from "../components/Modal";
 
 const ChoreCard = ({ chore }) => {
   const [checked, setChecked] = React.useState(chore.is_completed);
+  const [modalVisible, setModalVisible] = React.useState(false);
 
   const handleCheckbox = () => {
-      patchChoreIsCompleted(chore.chore_id, checked);
-      setChecked(!checked);
+    patchChoreIsCompleted(chore.chore_id, checked);
+    setChecked(!checked);
   };
 
   return (
-    // <Card>
-    //   <Card.Content>
-    <List.Item
-      title={chore.chore_name}
-      description={chore.description}
-      left={(props) => (
-        <Checkbox
-          status={checked ? "checked" : "unchecked"}
-          onPress={handleCheckbox}
-        />
-      )}
-      right={(props) => (
-        <Button mode="outlined" onPress={() => console.log("Pressed")}>
-          Info
-        </Button>
-      )}
-    />
+    <View>
+      <ChoreModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        chore={chore}
+      />
+      {/* <Card>
+       <Card.Content> */}
+      <List.Item
+        title={chore.chore_name}
+        description={chore.description}
+        left={(props) => (
+          <Checkbox
+            status={checked ? "checked" : "unchecked"}
+            onPress={handleCheckbox}
+          />
+        )}
+        right={(props) => (
+          <Button mode="outlined" onPress={() => setModalVisible(true)}>
+            Info
+          </Button>
+        )}
+      />
+    </View>
     //   </Card.Content>
     //   {/* <Card.Actions>
     //   <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
