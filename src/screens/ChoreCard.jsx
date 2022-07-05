4b521cf8f637787fb8c14ea2a398f138555d0e1f
Karly-22 <karly.holloway@hotmail.com>
2022-07-05 14:22:47 +0100

    add styling to chores list

--- src/screens/ChoreCard.jsx
+++ src/screens/ChoreCard.jsx
@@ -1,8 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import { Pressable, Button, Card, Checkbox, List } from "react-native-paper";
+import {
+  Button,
+  Card,
+  Checkbox,
+  List,
+} from "react-native-paper";
 import { patchChoreIsCompleted } from "../utils/api";
 import ChoreModal from "../components/Modal";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ChoreCard = ({ chore }) => {
   const [checked, setChecked] = React.useState(chore.is_completed);
@@ -14,38 +20,72 @@ const ChoreCard = ({ chore }) => {
   };
 
   return (
-    <View>
-      <ChoreModal
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}
-        chore={chore}
-      />
-      {/* <Card>
-       <Card.Content> */}
-      <List.Item
-        title={chore.chore_name}
-        description={chore.description}
-        left={(props) => (
-          <Checkbox
-            status={checked ? "checked" : "unchecked"}
-            onPress={handleCheckbox}
+    <SafeAreaView style={styles.container}>
+      <Card style={styles.choreCard}>
+        <ChoreModal
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          chore={chore}
+        />
+        <Card.Content style={styles.cardContent}>
+          <List.Item
+            title={chore.chore_name}
+            description={chore.description}
+            left={(props) => (
+              <Checkbox
+                status={checked ? "checked" : "unchecked"}
+                onPress={handleCheckbox}
+                style={styles.checkbox}
+              />
+            )}
+            right={(props) => (
+              <Button
+                style={styles.button}
+                mode="outlined"
+                onPress={() => setModalVisible(true)}
+              >
+                Info
+              </Button>
+            )}
+            style={styles.list}
           />
-        )}
-        right={(props) => (
-          <Button mode="outlined" onPress={() => setModalVisible(true)}>
-            Info
-          </Button>
-        )}
-      />
-    </View>
-    //   </Card.Content>
-    //   {/* <Card.Actions>
-    //   <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-    //   Press me
-    // </Button>
-    //   </Card.Actions> */}
-    // </Card>
+        </Card.Content>
+      </Card>
+    </SafeAreaView>
   );
 };
 
 export default ChoreCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginTop: 16,
+    alignItems: "center", // Centered horizontally
+  },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  //   color: Colors.white,
+  // },
+  choreCard: {
+    width: "100%",
+    padding: 0,
+    margin: 0,
+  },
+  cardContent: {
+  
+  },
+  list: {
+    padding: 0,
+    margin: 0,
+  },
+  checkbox: {
+    height: 16,
+  },
+  button: {
+    padding: 0,
+    margin: 0,
+  }
+});
