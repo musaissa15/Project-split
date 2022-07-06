afd4d2adb9bd9d754ed2e2b21a2dcbf5e51c9470
Karly-22 <karly.holloway@hotmail.com>
2022-07-06 16:33:09 +0100

    styling

--- src/screens/Chores.jsx
+++ src/screens/Chores.jsx
@@ -16,18 +16,48 @@ const Chores = () => {
   }, []);
 
   return (
-      <ScrollView style={styles.back}>
-      {householdChores.map((chore) => {
-        return <ChoreCard chore={chore} householdChores={householdChores} setHouseholdChores={setHouseholdChores} key={chore.chore_id} />;
-      })}
-      </ScrollView> 
+    <ScrollView style={styles.back}>
+      <View style={styles.banner}>
+        <Text style={[styles.cardContent, styles.heading]}>All chores</Text>
+      </View>
+      <View style={styles.listChores}>
+        {householdChores.map((chore) => {
+          return (
+            <ChoreCard
+              chore={chore}
+              householdChores={householdChores}
+              setHouseholdChores={setHouseholdChores}
+              key={chore.chore_id}
+            />
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
 
 export default Chores;
 
 const styles = StyleSheet.create({
-  back: {
+  banner: {
+    padding: 10,
+    width: "100%",
     backgroundColor: "#2F5D62",
+    height: 125,
+    alignItems: "center",
+  },
+  back: {
+    backgroundColor: "#DFEEEA",
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: "bold",
+    padding: 10,
+    marginTop: 10,
+    color: "white",
+    alignItems: "center",
+  },
+  listChores: {
+    marginTop: -60,
   }
 });
