a9dcf4878b45072b914b04951b00386857a4efef
Karly-22 <karly.holloway@hotmail.com>
2022-07-06 22:43:39 +0100

    remove useEffect

--- src/screens/Chores.jsx
+++ src/screens/Chores.jsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 import { getChoresByHouseholdId } from "../utils/api";
 import ChoreCard from "./ChoreCard";
